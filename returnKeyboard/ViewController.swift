eabbcdd69b30df663fbcd89fe6d1aee70ae1eefc
Apple Hsiao <iamap1123@gmail.com>
2016-11-07 14:07:26 +0800

    new

--- returnKeyboard/ViewController.swift
+++ returnKeyboard/ViewController.swift
@@ -8,8 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var aTextField: UITextField!
+    @IBOutlet weak var bTextField: UITextField!
+    @IBOutlet weak var combineLabel: UILabel!
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //textField.resignFirstResponder() 兩種寫法皆可
+        self.view.endEditing(true)
+        return true
+    }
+    
+    @IBAction func combineButton(_ sender: UIButton) {
+        if let textA = aTextField.text, let textB = bTextField.text{
+           combineLabel.text = textA + textB
+        }
+        self.view.endEditing(true)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
